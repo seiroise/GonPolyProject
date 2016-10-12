eb3ae0c6b4da98f2df822fdf634d8aa055105f98
seiroise <seiroise@gmail.com>
2016-10-12 10:54:57 +0900

    パーツ階層の表示_1

---
+++ Assets/Scripts/_Test/PolyPartsEditor/UI/Hierarchy/HierarchyUIManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Scripts._Test.PolyPartsEditor.UI.Hierarchy {
+
+	/// <summary>
+	/// パーツ階層のUI表示
+	/// </summary>
+	class HierarchyUIManager : MonoBehaviour {
+		
+	}
+}
